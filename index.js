2b16a2d4408818ce21b997d5ded88df899b0bd58
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-04-26 11:31:57 +1000

    init commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// import all user routes (up top in index.js)
+const userRoutes = require('./routes/userRoutes.js');
+// map the user routes to our app
+app.use('/users', userRoutes);
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -13,7 +18,8 @@ at http://localhost:${port}`)
 
 
 // import all calculator routes (up the top)
-import calculatorRoutes from './Routes/calculatorRoutes'
+import calculatorRoutes from './Routes/calculatorRoutes.js';
+
 
 // map the calculator routes to our app
 app.use('/calculator', calculatorRoutes);
